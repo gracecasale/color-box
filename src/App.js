ddd5a0f8d04695c705ec1368571d72b7805ff057
Grace Casale <gracecasale@gmail.com>
2018-10-31 18:42:18 -0400

    created the skeleton for the app component structure

--- src/App.js
+++ src/App.js
@@ -5,7 +5,20 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello! It works!</h1>
+        <header className="App-header">
+          <h1 className="title">Color Box</h1>
+        </header>
+
+        <main className="App-main">
+          <div className="box">
+            <input type="color" value="#ff0000" className="color-selector" />
+          </div>
+        </main>
+
+        <footer className="App-footer">
+          <p>Made by Grace Casale</p>
+        </footer>
+
       </div>
     );
   }
